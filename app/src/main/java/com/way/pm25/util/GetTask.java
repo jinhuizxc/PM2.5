efaec98e1f2c9b37ebb750a5042aa62c34bde567
jinhuizxc <1004260403@qq.com>
2017-03-10 00:18:47 +0800

    Initial commit

---
+++ app/src/main/java/com/way/pm25/util/GetTask.java
@@ -0,0 +1,16 @@
+package com.way.pm25.util;
+
+import android.os.AsyncTask;
+
+public class GetTask extends AsyncTask<String, String, String> {
+	private String mTargetURI;
+
+	public GetTask(String uri) {
+		this.mTargetURI = uri;
+	}
+
+	protected String doInBackground(String[] strs) {
+		APIClient apiClient = new APIClient();
+		return apiClient.request(String.format(mTargetURI, strs[0]));
+	}
+}
